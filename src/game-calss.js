f549a54d23e41bff02a952d95cb63767da23dea4
Amine <ouas.amine@outlook.com>
2022-10-12 16:10:55 +0100

    Make Game class methods async & inject the fetched data into the DOM

--- src/game-calss.js
+++ src/game-calss.js
@@ -1,29 +1,25 @@
-class Game {
+export default class Game {
   constructor() {
     this.baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/AILK8GYEBS5QEfR1WfuU/';
   }
 
-  addScore(endPoint, userName, userScore) {
+  async addScore(endPoint, userName, userScore) {
     const userObj = { user: userName, score: userScore };
     const fetchingURL = this.baseURL + endPoint;
 
-    fetch(fetchingURL, {
+    const result = await fetch(fetchingURL, {
       method: 'POST',
       headers: {
         'Content-type': 'application/json; charset=UTF-8',
       },
       body: JSON.stringify(userObj),
-    })
-      .then((response) => response.json())
-      .then((json) => console.log(json));
+    });
+    return result;
   }
 
-  getScores(endPoint) {
+  async getScores(endPoint) {
     const fetchingURL = this.baseURL + endPoint;
-
-    fetch(fetchingURL)
-      .then((response) => response.json())
-      .then((json) => console.log(json));
+    const result = await fetch(fetchingURL);
+    return result;
   }
 }
-
--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 import './style.css';
-import './game-calss.js';
+import Game from './game-calss.js';
 
+const newGame = new Game();
+const formUserInput = document.forms['new-score']['user-name'];
+const formscoreInput = document.forms['new-score']['user-score'];
+const submitBtt = document.querySelector('#add-score');
+const scoresList = document.querySelector('#list-column ul');
+const messageContainer = document.querySelector('.message');
 
+const diplayList = (scoresArray) => {
+  scoresArray.forEach((arrayObj) => {
+    const listItem = document.createElement('li');
+    listItem.innerText = `${arrayObj.user}: ${arrayObj.score}`;
+    scoresList.append(listItem);
+  });
+};
 
+newGame.getScores('scores/')
+  .then((res) => res.json())
+  .then((json) => {
+    diplayList(json.result);
+  });
+submitBtt.addEventListener('click', (event) => {
+  event.preventDefault();
+  const userName = formUserInput.value;
+  const userScore = formscoreInput.value;
+  newGame.addScore('scores/', userName, userScore)
+    .then(() => {
+      messageContainer.innerText = 'The score has been successfully added';
+    });
+});
